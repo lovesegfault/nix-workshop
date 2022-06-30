6268bd437f94084ca87473b3d84f21743421c23d
hiljusti <hiljusti@amazon.com>
2022-06-30 16:55:11 -0700

    feat: add hello-cobol

---
+++ hello-cobol/hello.cbl
@@ -0,0 +1,23 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Hello.
+
+       ENVIRONMENT DIVISION.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 ArgLen PIC 9(4).
+       01 Name PIC X(32).
+
+       PROCEDURE DIVISION.
+
+       ACCEPT ArgLen FROM ARGUMENT-NUMBER.
+       IF ArgLen = 0 THEN
+         MOVE "Cobol world" TO Name
+       ELSE
+         ACCEPT Name FROM ARGUMENT-VALUE
+       END-IF
+
+       DISPLAY "Hello " FUNCTION TRIM(Name) "!".
+
+       EXIT PROGRAM.
+
