698001335f770bb2d5d2c6e43b42547042c27f74
Bernardo Meurer <bernardo@meurer.org>
2022-07-01 10:40:24 -0400

    refactor(flake): appease ci checks

--- flake.nix
+++ flake.nix
@@ -7,73 +7,97 @@
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
 
-  outputs = { self, flake-utils, nixpkgs, poetry2nix }:
-  let
-    supportedSystems = [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
-  in
-  flake-utils.lib.eachSystem supportedSystems (system:
+  outputs = { self, flake-utils, nixpkgs, poetry2nix, pre-commit-hooks }:
     let
-      system = "aarch64-darwin";
-
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          poetry2nix.overlay
-        ];
-      };
-      inherit (pkgs) lib;
-
-      helloPkgs = lib.filterAttrs (n: v: lib.hasPrefix "hello-" n) self.packages.${system};
+      supportedSystems = [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ];
     in
-    {
-      packages = {
-        default = pkgs.symlinkJoin {
-          name = "nix-workshop";
-          paths = lib.attrValues helloPkgs;
+    flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        system = "aarch64-darwin";
+
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            poetry2nix.overlay
+          ];
         };
-        hello-c = pkgs.callPackage ./hello-c { };
-        hello-poetry = pkgs.callPackage ./hello-poetry { };
-        hello-rust = pkgs.callPackage ./hello-rust { };
-        hello-go = pkgs.callPackage ./hello-go { };
-        hello-setuptools = pkgs.python3Packages.callPackage ./hello-setuptools { };
-        dockerImages = lib.mapAttrs
-          (n: v: pkgs.dockerTools.buildImage {
-            name = "${n}-docker";
-            config.Cmd = [ "${v}/bin/${n}" ];
-          })
-          helloPkgs;
-      };
+        inherit (pkgs) lib;
 
-      devShells.default = pkgs.mkShell {
-        name = "nix-workshop";
+        helloPkgs = lib.filterAttrs (n: _: lib.hasPrefix "hello-" n) self.packages.${system};
+      in
+      {
+        packages = {
+          default = pkgs.symlinkJoin {
+            name = "nix-workshop";
+            paths = lib.attrValues helloPkgs;
+          };
+          hello-c = pkgs.callPackage ./hello-c { };
+          hello-poetry = pkgs.callPackage ./hello-poetry { };
+          hello-rust = pkgs.callPackage ./hello-rust { };
+          hello-go = pkgs.callPackage ./hello-go { };
+          hello-setuptools = pkgs.python3Packages.callPackage ./hello-setuptools { };
+          docker-image = pkgs.dockerTools.buildImage {
+            name = "hello-docker";
+            contents = lib.attrValues helloPkgs;
+            config.Cmd =
+              let
+                runner = lib.concatMapStringsSep " && " (n: "/bin/${n}") (lib.attrNames helloPkgs);
+              in
+              [ runner ];
+          };
+        };
 
-        inputsFrom = lib.attrValues helloPkgs;
+        devShells.default = pkgs.mkShell {
+          name = "nix-workshop";
 
-        nativeBuildInputs = with pkgs; [
-          # C
-          clang-tools
-          cppcheck
-          # Nix
-          nix-linter
-          nix-tree
-          nixpkgs-fmt
-          rnix-lsp
-          # Python
-          pyright
-          # Rust
-          cargo-edit
-          cargo-fuzz
-          rust-analyzer
-          # Shell
-          shellcheck
-          shfmt
-          # Generic
-          graphviz
-        ];
+          inputsFrom = (lib.attrValues helloPkgs)
+            ++ (lib.attrValues self.checks.${system});
 
-        buildInputs = with pkgs; [ ];
-      };
-    });
-  }
+          nativeBuildInputs = with pkgs; [
+            # C
+            clang-tools
+            cppcheck
+            # Nix
+            nix-linter
+            nix-tree
+            nixpkgs-fmt
+            rnix-lsp
+            # Python
+            pyright
+            # Rust
+            cargo-edit
+            cargo-fuzz
+            rust-analyzer
+            # Shell
+            shellcheck
+            shfmt
+            # Generic
+            graphviz
+          ];
+
+          buildInputs = with pkgs; [ ];
+        };
+
+        checks.pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = lib.cleanSource ./.;
+          hooks = {
+            nix-linter.enable = true;
+            nixpkgs-fmt.enable = true;
+            statix.enable = true;
+            actionlint = {
+              enable = true;
+              files = "^.github/workflows/";
+              types = [ "yaml" ];
+              entry = lib.getExe pkgs.actionlint;
+            };
+          };
+        };
+      });
+}
